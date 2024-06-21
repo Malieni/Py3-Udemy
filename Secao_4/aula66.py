867560b9c240be376f1f6754ed7c2a8818e48a0e
Malieni <gui.souza.malieni@gmail.com>
2024-06-21 11:32:36 -0300

    progresso secao_4

--- Secao_4/aula66.py
+++ Secao_4/aula66.py
@@ -3,3 +3,12 @@ Argumentos nomeados e não nomeados em funçoes Python
 Argumento nomeado tem nome com sinal de igual
 Argumento não nomeado recente apenas o argumento (valor)
 """
+def soma(x , y , z):
+    #definição
+    print(f'{x=} + y={y} y={z}' , '|',  'x + y = z =' , x + y + z)
+
+
+soma(1, 2 , 3)
+soma(1, y=2, z=5)
+
+print(1 ,2 ,3 , sep='-')
---
+++ Secao_4/aula69.py
@@ -0,0 +1,34 @@
+""" 
+Escopo em Python 
+Escopo siginifica local onde aquele código pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local 
+podem ser alcançados.  
+Não temos acesso a nomes de escopos internos nos escopos
+externos.
+a palavra global faz uma variável do escopo externo 
+ser a mesma no escopo interno.
+É possível alterar variáveis de escopo externo em escopo interno mas É UMA MÁ PRÁTICA
+"""
+
+x = 1
+
+def escopo():
+    #global x
+    x = 10 
+
+
+    def outra_funcao():
+        #global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    
+    print(x)
+
+print(x)
+escopo()
+print(x)
